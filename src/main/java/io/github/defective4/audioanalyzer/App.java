fd4285e6f49ae543e33e14fdf032ebee09c41293
Defective <def3ctive4@gmail.com>
2026-06-09 21:17:29 +0200

    Separate application logic to a new class

---
+++ src/main/java/io/github/defective4/audioanalyzer/App.java
@@ -0,0 +1,141 @@
+package io.github.defective4.audioanalyzer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.defective4.audioanalyzer.ml.Database;
+import io.github.defective4.audioanalyzer.ml.ModelLoader;
+import io.github.defective4.audioanalyzer.ml.TensorflowAnalyzer;
+import io.github.defective4.audioanalyzer.ml.model.AnalysisResponse;
+import io.github.defective4.audioanalyzer.ml.model.ModelMetadata;
+import io.github.defective4.audioanalyzer.ml.model.Track;
+import io.github.defective4.audioanalyzer.subsonic.SubsonicAPI;
+import io.github.defective4.audioanalyzer.subsonic.model.Entity;
+
+public class App {
+
+    private final String analyzerURL;
+
+    private final SubsonicAPI api;
+    private final Database db;
+    private final Logger logger = LoggerFactory.getLogger(CLI.class);
+    public App(String jdbcURL, String username, char[] password, String url, String analyzerURL)
+            throws SQLException, IOException {
+        this.analyzerURL = analyzerURL;
+        db = new Database(jdbcURL);
+        api = new SubsonicAPI(username, password, url);
+    }
+
+    public void groupTracks() throws SQLException {
+        // checkAPI();
+        int limit = 30;
+        String id = "エキストラ·ヒーロー";
+        Optional<Track> baseOp = Optional.empty();
+        baseOp = db.getTrackById(id);
+        if (!baseOp.isPresent()) {
+            logger.error("Track with id or name %s does not exist.");
+            return;
+        }
+
+        Track base = baseOp.get();
+
+        List<Track> similar = db.getAllTracks().stream().sorted((t1, t2) -> {
+            double diff = calculateSimilarity(t1, base) - calculateSimilarity(t2, base);
+            return diff < 0 ? -1 : diff > 0 ? 1 : 0;
+        }).filter(track -> !track.id().equals(base.id())).limit(limit).toList();
+
+        System.out.println(similar.stream().map(Track::name).toList());
+    }
+
+    public void index(boolean onlyNew) throws Exception {
+        try {
+            TensorflowAnalyzer analyzer = getTensorflow();
+            logger.info("Analyzer server OK");
+            ModelLoader modelLoader = new ModelLoader(Path.of("./models/other"), logger);
+            Map<String, ModelMetadata> models = modelLoader.getLoadedModels();
+            checkAPI();
+            logger.info("Downloading track lists...");
+            List<Entity> songs = api.getAllMusic(logger);
+            logger.info("Downloaded information about %s songs".formatted(songs.size()));
+            logger.info("Starting analysis...");
+
+            List<String> ignore = onlyNew ? db.getAllSongIDs() : List.of();
+
+            Path tmpDir = Files.createTempDirectory("e-analysis-");
+            tmpDir.toFile().deleteOnExit();
+
+            for (int i = 0; i < songs.size(); i++) {
+                Entity song = songs.get(i);
+                if (ignore.contains(song.id())) {
+                    logger.info("%s is already in database, skipped.".formatted(song.title()));
+                    continue;
+                }
+                logger.info("Starting analysis of %s (%s) [%s/%s]...".formatted(song.title(), song.id(), i + 1,
+                        songs.size()));
+                logger.info("Downloading %s...".formatted(song.id()));
+                Path target = Path.of(tmpDir.toString(), Path.of(song.path()).getFileName().toString());
+                target.toFile().deleteOnExit();
+                try {
+                    try (InputStream in = api.download(song)) {
+                        Files.copy(in, target);
+                    }
+                    logger.info("Analyzing %s...".formatted(song.id()));
+                    AnalysisResponse response = analyzer.requestAnalysis(target.toString());
+                    logger.info("Storing results in database...");
+                    logger.info("Results for %s:".formatted(song.title()));
+                    String moodName = models.get("moods").classes()[response.mood()];
+                    String instrumentName = models.get("instruments").classes()[response.instrument()];
+                    String genreName = models.get("genres").classes()[response.genre()];
+
+                    logger.info(" Mood: %s".formatted(moodName));
+                    logger.info(" Instrument: %s".formatted(instrumentName));
+                    logger.info(" Genre: %s".formatted(genreName));
+                    System.err.println();
+                    db.insertData(song, response.scoreMap(), moodName, instrumentName, genreName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    continue;
+                } finally {
+                    target.toFile().delete();
+                }
+            }
+        } catch (Exception e) {
+            logger.error("An error occured: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    private void checkAPI() throws IOException {
+        logger.info("Checking credentials...");
+        api.ping();
+        logger.info("Logged in successfully!");
+    }
+
+    private TensorflowAnalyzer getTensorflow() throws MalformedURLException, IOException {
+        logger.info("Pinging analyzer server...");
+        TensorflowAnalyzer analyzer = new TensorflowAnalyzer(analyzerURL);
+        analyzer.ping();
+        return analyzer;
+    }
+
+    private static double calculateSimilarity(Track track1, Track track2) {
+        double sum = 0;
+        for (Entry<String, Float> entry : track1.scores().entrySet()) {
+            float diff = entry.getValue() - track2.scores().get(entry.getKey());
+            sum += diff * diff;
+        }
+        return Math.sqrt(sum);
+    }
+
+}
--- src/main/java/io/github/defective4/audioanalyzer/CLI.java
+++ src/main/java/io/github/defective4/audioanalyzer/CLI.java
@@ -1,16 +1,8 @@
 package io.github.defective4.audioanalyzer;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
@@ -19,22 +11,11 @@ import org.apache.commons.cli.MissingArgumentException;
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.github.defective4.audioanalyzer.ml.Database;
-import io.github.defective4.audioanalyzer.ml.ModelLoader;
-import io.github.defective4.audioanalyzer.ml.TensorflowAnalyzer;
-import io.github.defective4.audioanalyzer.ml.model.AnalysisResponse;
-import io.github.defective4.audioanalyzer.ml.model.ModelMetadata;
-import io.github.defective4.audioanalyzer.ml.model.Track;
-import io.github.defective4.audioanalyzer.subsonic.SubsonicAPI;
-import io.github.defective4.audioanalyzer.subsonic.model.Entity;
 
 public class CLI {
 
     private interface CLIConsumer {
-        void consume(CommandLine cli, CLI prog) throws Exception;
+        void consume(CommandLine cli, App prog) throws Exception;
 
         String desc();
 
@@ -46,7 +27,7 @@ public class CLI {
     private static final Map<String, CLIConsumer> COMMANDS = Map.of("analyze", new CLIConsumer() {
 
         @Override
-        public void consume(CommandLine cli, CLI prog) throws Exception {
+        public void consume(CommandLine cli, App prog) throws Exception {
             prog.index(!cli.hasOption('n'));
         }
 
@@ -61,7 +42,7 @@ public class CLI {
         }
     }, "generate-playlist", new CLIConsumer() {
         @Override
-        public void consume(CommandLine cli, CLI prog) throws Exception {
+        public void consume(CommandLine cli, App prog) throws Exception {
             prog.groupTracks();
         }
 
@@ -101,111 +82,6 @@ public class CLI {
                         .numberOfArgs(1).argName("url").build());
         PLAYLIST_OPTIONS = new Options().addOptions(COMMON_OPTIONS);
     }
-    private final String analyzerURL;
-
-    private final SubsonicAPI api;
-
-    private final Database db;
-    private final Logger logger = LoggerFactory.getLogger(CLI.class);
-
-    public CLI(String jdbcURL, String username, char[] password, String url, String analyzerURL)
-            throws SQLException, IOException {
-        this.analyzerURL = analyzerURL;
-        db = new Database(jdbcURL);
-        api = new SubsonicAPI(username, password, url);
-    }
-
-    private void checkAPI() throws IOException {
-        logger.info("Checking credentials...");
-        api.ping();
-        logger.info("Logged in successfully!");
-    }
-
-    private TensorflowAnalyzer getTensorflow() throws MalformedURLException, IOException {
-        logger.info("Pinging analyzer server...");
-        TensorflowAnalyzer analyzer = new TensorflowAnalyzer(analyzerURL);
-        analyzer.ping();
-        return analyzer;
-    }
-
-    private void groupTracks() throws SQLException {
-//        checkAPI();
-        int limit = 30;
-        String id = "エキストラ·ヒーロー";
-        Optional<Track> baseOp = Optional.empty();
-        baseOp = db.getTrackById(id);
-        if (!baseOp.isPresent()) {
-            logger.error("Track with id or name %s does not exist.");
-            return;
-        }
-
-        Track base = baseOp.get();
-
-        List<Track> similar = db.getAllTracks().stream().sorted((t1, t2) -> {
-            double diff = calculateSimilarity(t1, base) - calculateSimilarity(t2, base);
-            return diff < 0 ? -1 : diff > 0 ? 1 : 0;
-        }).filter(track -> !track.id().equals(base.id())).limit(limit).toList();
-
-        System.out.println(similar.stream().map(Track::name).toList());
-    }
-
-    private void index(boolean onlyNew) throws Exception {
-        try {
-            TensorflowAnalyzer analyzer = getTensorflow();
-            logger.info("Analyzer server OK");
-            ModelLoader modelLoader = new ModelLoader(Path.of("./models/other"), logger);
-            Map<String, ModelMetadata> models = modelLoader.getLoadedModels();
-            checkAPI();
-            logger.info("Downloading track lists...");
-            List<Entity> songs = api.getAllMusic(logger);
-            logger.info("Downloaded information about %s songs".formatted(songs.size()));
-            logger.info("Starting analysis...");
-
-            List<String> ignore = onlyNew ? db.getAllSongIDs() : List.of();
-
-            Path tmpDir = Files.createTempDirectory("e-analysis-");
-            tmpDir.toFile().deleteOnExit();
-
-            for (int i = 0; i < songs.size(); i++) {
-                Entity song = songs.get(i);
-                if (ignore.contains(song.id())) {
-                    logger.info("%s is already in database, skipped.".formatted(song.title()));
-                    continue;
-                }
-                logger.info("Starting analysis of %s (%s) [%s/%s]...".formatted(song.title(), song.id(), i + 1,
-                        songs.size()));
-                logger.info("Downloading %s...".formatted(song.id()));
-                Path target = Path.of(tmpDir.toString(), Path.of(song.path()).getFileName().toString());
-                target.toFile().deleteOnExit();
-                try {
-                    try (InputStream in = api.download(song)) {
-                        Files.copy(in, target);
-                    }
-                    logger.info("Analyzing %s...".formatted(song.id()));
-                    AnalysisResponse response = analyzer.requestAnalysis(target.toString());
-                    logger.info("Storing results in database...");
-                    logger.info("Results for %s:".formatted(song.title()));
-                    String moodName = models.get("moods").classes()[response.mood()];
-                    String instrumentName = models.get("instruments").classes()[response.instrument()];
-                    String genreName = models.get("genres").classes()[response.genre()];
-
-                    logger.info(" Mood: %s".formatted(moodName));
-                    logger.info(" Instrument: %s".formatted(instrumentName));
-                    logger.info(" Genre: %s".formatted(genreName));
-                    System.err.println();
-                    db.insertData(song, response.scoreMap(), moodName, instrumentName, genreName);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    continue;
-                } finally {
-                    target.toFile().delete();
-                }
-            }
-        } catch (Exception e) {
-            logger.error("An error occured: " + e.getMessage());
-            throw e;
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         if (args.length < 1 || !COMMANDS.containsKey(args[0])) {
@@ -229,7 +105,7 @@ public class CLI {
                 String essentiaURL = cli.hasOption('t') ? cli.getOptionValue('t') : DEFAULT_ESSENTIA;
                 if (!essentiaURL.endsWith("/")) essentiaURL = essentiaURL + "/";
 
-                CLI prog = new CLI(jdbc, user, password.toCharArray(), subsonicURL, essentiaURL);
+                App prog = new App(jdbc, user, password.toCharArray(), subsonicURL, essentiaURL);
                 COMMANDS.get(args[0]).consume(cli, prog);
                 return;
             }
@@ -241,14 +117,5 @@ public class CLI {
                 Path.of(CLI.class.getProtectionDomain().getCodeSource().getLocation().getFile()).getFileName(),
                 args[0]), options);
         System.exit(2);
-    }
-
-    private static double calculateSimilarity(Track track1, Track track2) {
-        double sum = 0;
-        for (Entry<String, Float> entry : track1.scores().entrySet()) {
-            float diff = entry.getValue() - track2.scores().get(entry.getKey());
-            sum += diff * diff;
-        }
-        return Math.sqrt(sum);
     }
 }
